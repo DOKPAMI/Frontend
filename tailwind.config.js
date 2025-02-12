95180a92fdf5d17d1140ee74cb85596e12a08cc7
ddol9 <drddol9@gmail.com>
2025-02-12 15:01:18 +0900

    chore: add shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,21 @@
 import { addDynamicIconSelectors } from '@iconify/tailwind'
 
 export default {
+  darkMode: ['class'],
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {},
+    },
   },
   plugins: [
     // Iconify plugin
     addDynamicIconSelectors(),
+    require('tailwindcss-animate'),
   ],
 }
