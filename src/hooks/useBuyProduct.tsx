f80a04c797226f27cd6dd06559ffc12ad283f811
guboc11 <guboc11@gmail.com>
2025-05-21 03:20:43 +0900

    Fix Event page hook issue

--- src/hooks/useBuyProduct.tsx
+++ src/hooks/useBuyProduct.tsx
@@ -1,10 +1,9 @@
 import { useCurrentAccount, useSignAndExecuteTransaction } from '@mysten/dapp-kit';
 import { Transaction } from '@mysten/sui/transactions';
-import { useToast } from './useToast';
+import { toast } from 'sonner';
 
 export function useBuyProduct() {
   const packageId = '0x07a5e74b3b575404a50b0de20002b183f118301cf37e308d3770634ec047a1f8';
-  const { setToastState } = useToast();
   const { mutate: signAndExecuteTransaction } = useSignAndExecuteTransaction();
   const account = useCurrentAccount();
 
@@ -19,10 +18,8 @@ export function useBuyProduct() {
   }) => {
     const collectionId = '0x1731b154ac970f4e301a996b5cacc58f8b96c4971e1ff257a8a25bea2d457866';
 
-    setToastState({
-      type: 'loading',
-      message: 'Request is being created...',
-    });
+    toast.dismiss();
+    toast.loading('Loading...');
 
     const tx = new Transaction();
     const [request] = tx.moveCall({
@@ -72,19 +69,13 @@ export function useBuyProduct() {
       },
       {
         onSuccess: (data) => {
-          console.log('Success! data:', data);
-          setToastState({
-            type: 'success',
-            message: 'Buying product succeeded.',
-          });
+          toast.dismiss();
+          toast.success(`Sucdess! digest: ${data.digest}`);
           setStatus('downloaded');
         },
         onError: (err) => {
-          console.log('Error', err);
-          setToastState({
-            type: 'error',
-            message: 'Something went wrong while buying the product. Please try again.',
-          });
+          toast.dismiss();
+          toast.error(`Error: ${err}`);
         },
       },
     );
--- src/pages/Event/Event.tsx
+++ src/pages/Event/Event.tsx
@@ -3,20 +3,28 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { characters } from './characters';
-import { useConnectWallet, useWallets, useCurrentAccount } from '@mysten/dapp-kit';
+import {
+  useConnectWallet,
+  useWallets,
+  useCurrentAccount,
+  useDisconnectWallet,
+} from '@mysten/dapp-kit';
 import { ZkSendLink } from '@mysten/zksend';
 import { claimAssets } from '@/lib/zksend';
 import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
 import { useBuyProduct } from '@/hooks/useBuyProduct';
+import { toast } from 'sonner';
 const Event = () => {
   const [selectedCharacter, setSelectedCharacter] = useState(0);
-  const [status, setStatus] = useState<'default' | 'loggedIn' | 'downloaded'>('default');
   const [showDetail, setShowDetail] = useState(false);
-  const [isClaiming, setIsClaiming] = useState(false);
   const [language, setLanguage] = useState<'ko' | 'en'>('ko');
+  const [isNFTMinted, setIsNFTMinted] = useState(false);
+
   const wallets = useWallets();
-  const { mutate: connect } = useConnectWallet();
   const account = useCurrentAccount();
+
+  const { mutate: connect } = useConnectWallet();
+  const { mutate: disconnect } = useDisconnectWallet();
   const { buyEventProduct } = useBuyProduct();
 
   const settings = {
@@ -30,44 +38,6 @@ const Event = () => {
     beforeChange: (_: number, next: number) => setSelectedCharacter(next),
   };
 
-  const handleButtonClick = () => {
-    if (status === 'default') {
-      setIsClaiming(true);
-      connect(
-        { wallet: wallets[0] },
-        {
-          onSuccess: async () => {
-            const claimLink = await fetch(
-              `${import.meta.env.VITE_BACKEND_URL}/zk/zk-send/mintPami`,
-              {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                  recipient: account?.address || '',
-                }),
-              },
-            );
-            const claimLinkData = await claimLink.json();
-            console.log(claimLinkData);
-            const link = await ZkSendLink.fromUrl(claimLinkData);
-            const tx = await link.createClaimTransaction(account?.address!);
-            await claimAssets(tx, account?.address!, link.keypair as Ed25519Keypair);
-            setStatus('loggedIn');
-            setIsClaiming(false);
-          },
-        },
-      );
-    } else if (status === 'loggedIn') {
-      buyEventProduct({
-        storeId: characters[selectedCharacter].storeId,
-        slotNumber: characters[selectedCharacter].slotNumber,
-        setStatus: () => setStatus('downloaded'),
-      });
-    } else if (status === 'downloaded') {
-      downloadImage();
-    }
-  };
-
   const downloadImage = () => {
     const selectedChar = characters[selectedCharacter];
     const link = document.createElement('a');
@@ -78,44 +48,18 @@ const Event = () => {
     document.body.removeChild(link);
   };
 
-  const getButtonText = () => {
-    if (language === 'en') {
-      switch (status) {
-        case 'default':
-          return 'Login with Google';
-        case 'loggedIn':
-          return 'Get NFT';
-        case 'downloaded':
-          return 'Download Image';
-      }
-    } else {
-      switch (status) {
-        case 'default':
-          return '구글 로그인하기';
-        case 'loggedIn':
-          return 'NFT 발급하기';
-        case 'downloaded':
-          return '이미지 다운받기';
-      }
-    }
-  };
-
   const getDescriptionText = () => {
     if (language === 'en') {
-      switch (status) {
-        case 'default':
-        case 'loggedIn':
-          return 'Choose your favorite Pamhee NFT\nand get a free Monster Energy!';
-        case 'downloaded':
-          return 'Congratulations — your Pamhee NFT has been successfully minted!\nIt’s now live on the Sui blockchain.\nView it in your Slush Wallet or on the Dokpamhee page.';
+      if (account && !isNFTMinted) {
+        return 'Choose your favorite Pamhee NFT\nand get a free Monster Energy!';
+      } else if (account && isNFTMinted) {
+        return 'Congratulations — your Pamhee NFT has been successfully minted!\nIt’s now live on the Sui blockchain.\nView it in your Slush Wallet or on the Dokpamhee page.';
       }
     } else {
-      switch (status) {
-        case 'default':
-        case 'loggedIn':
-          return '원하는 타입의 팜희 NFT를 발급받으면\n 몬스터 에너지를 받을 수 있어요.';
-        case 'downloaded':
-          return 'NFT 발급이 완료되었어요!\n발급한 NFT는 수이 블록체인에 기록되어 있어요. Slush Wallet이나 독팜희 페이지에서 NFT를 확인할 수 있어요.';
+      if (account && !isNFTMinted) {
+        return '원하는 타입의 팜희 NFT를 발급받으면\n 몬스터 에너지를 받을 수 있어요.';
+      } else if (account && isNFTMinted) {
+        return 'NFT 발급이 완료되었어요!\n발급한 NFT는 수이 블록체인에 기록되어 있어요. Slush Wallet이나 독팜희 페이지에서 NFT를 확인할 수 있어요.';
       }
     }
   };
@@ -229,20 +173,115 @@ const Event = () => {
         {/* 하단 버튼 */}
 
         <div className='absolute bottom-[20vh] xs:bottom-[10vh] sm:bottom-[4vh] md:bottom-[3vh] w-full flex justify-center px-4'>
-          <button
-            onClick={handleButtonClick}
-            disabled={isClaiming}
-            className='w-full max-w-[320px] h-10 xs:h-12 relative bg-amber-200 rounded-[73px] shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)] outline-2 outline-offset-[-2px] outline-black overflow-hidden flex items-center justify-center
+          {!account && (
+            <button
+              onClick={() => {
+                const wallet = wallets.find((w) => w.name.includes('Slush'))
+                  ? wallets.find((w) => w.name.includes('Slush'))
+                  : wallets[0];
+                // setIsClaiming(true);
+                if (wallet) {
+                  connect({ wallet });
+                }
+              }}
+              className='w-full max-w-[320px] h-10 xs:h-12 relative bg-amber-200 rounded-[73px] shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)] outline-2 outline-offset-[-2px] outline-black overflow-hidden flex items-center justify-center
+             disabled:cursor-not-allowed cursor-pointer disabled:bg-gray-300'
+            >
+              <span className='text-sm xs:text-base sm:text-lg md:text-xl font-bold font-["Jua"] leading-none translate-y-[1px] whitespace-nowrap px-2'>
+                {language === 'en' ? 'Login with Google' : '구글 로그인하기'}
+              </span>
+            </button>
+          )}
+          {account && (
+            <button
+              onClick={() => {
+                const wallet = wallets.find((w) => w.name.includes('Slush'))
+                  ? wallets.find((w) => w.name.includes('Slush'))
+                  : wallets[0];
+
+                if (wallet) {
+                  disconnect();
+                }
+              }}
+              className='w-full max-w-[320px] h-10 xs:h-12 relative bg-amber-200 rounded-[73px] shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)] outline-2 outline-offset-[-2px] outline-black overflow-hidden flex items-center justify-center
+             disabled:cursor-not-allowed cursor-pointer disabled:bg-gray-300'
+            >
+              <span className='text-sm xs:text-base sm:text-lg md:text-xl font-bold font-["Jua"] leading-none translate-y-[1px] whitespace-nowrap px-2'>
+                {language === 'en' ? 'Logout' : '로그아웃 하기'}
+              </span>
+            </button>
+          )}
+          {account && !localStorage.getItem('isClaimed') && (
+            <button
+              onClick={async () => {
+                toast.dismiss();
+                toast.loading('Processing... Please Wait..');
+
+                const claimLink = await fetch(
+                  `${import.meta.env.VITE_BACKEND_URL}/zk/zk-send/mintPami`,
+                  {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                      recipient: account?.address || '',
+                    }),
+                  },
+                );
+                const claimLinkData = await claimLink.json();
+
+                toast.dismiss();
+                toast.loading(`Getting claim link data...`);
+
+                const link = await ZkSendLink.fromUrl(claimLinkData);
+                const tx = link.createClaimTransaction(account?.address!);
+                await claimAssets(tx, account?.address!, link.keypair as Ed25519Keypair);
+
+                localStorage.setItem('isClaimed', 'true');
+
+                toast.dismiss();
+                toast.success('Claim Successful');
+              }}
+              className='cursor-pointer w-full max-w-[320px] h-10 xs:h-12 relative bg-amber-200 rounded-[73px] shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)] outline-2 outline-offset-[-2px] outline-black overflow-hidden flex items-center justify-center
              disabled:cursor-not-allowed disabled:bg-gray-300'
-          >
-            <span className='text-sm xs:text-base sm:text-lg md:text-xl font-bold font-["Jua"] leading-none translate-y-[1px] whitespace-nowrap px-2'>
-              {isClaiming
-                ? language === 'en'
-                  ? 'Setting up for NFT minting...'
-                  : 'NFT 발급을 위한 세팅중...'
-                : getButtonText()}
-            </span>
-          </button>
+            >
+              <span className='text-sm xs:text-base sm:text-lg md:text-xl font-bold font-["Jua"] leading-none translate-y-[1px] whitespace-nowrap px-2'>
+                {language === 'en' ? 'Claim Testnet SUI' : 'Testnet SUI 받기'}
+              </span>
+            </button>
+          )}
+
+          {account && localStorage.getItem('isClaimed') && !isNFTMinted && (
+            <button
+              onClick={() => {
+                buyEventProduct({
+                  storeId: characters[selectedCharacter].storeId,
+                  slotNumber: characters[selectedCharacter].slotNumber,
+                  setStatus: () => setIsNFTMinted(true),
+                });
+              }}
+              className='cursor-pointer w-full max-w-[320px] h-10 xs:h-12 relative bg-amber-200 rounded-[73px] shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)] outline-2 outline-offset-[-2px] outline-black overflow-hidden flex items-center justify-center
+             disabled:cursor-not-allowed disabled:bg-gray-300'
+            >
+              <span className='text-sm xs:text-base sm:text-lg md:text-xl font-bold font-["Jua"] leading-none translate-y-[1px] whitespace-nowrap px-2'>
+                {language === 'en' ? 'Get NFT' : 'NFT 발급하기'}
+              </span>
+            </button>
+          )}
+
+          {account && localStorage.getItem('isClaimed') && isNFTMinted && (
+            <button
+              onClick={() => {
+                downloadImage();
+                setIsNFTMinted(false);
+              }}
+              className='cursor-pointer w-full max-w-[320px] h-10 xs:h-12 relative bg-amber-200 rounded-[73px] shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)] outline-2 outline-offset-[-2px] outline-black overflow-hidden flex items-center justify-center
+             disabled:cursor-not-allowed disabled:bg-gray-300'
+            >
+              <span className='text-sm xs:text-base sm:text-lg md:text-xl font-bold font-["Jua"] leading-none translate-y-[1px] whitespace-nowrap px-2'>
+                {language === 'en' ? 'Download Image' : '이미지 다운받기'}
+              </span>
+            </button>
+          )}
         </div>
 
         {/* 상세보기 모달 */}
