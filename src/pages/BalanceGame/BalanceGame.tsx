5599dfd30187138c834e7f7cb37727486acdd2e7
poqopo <block.poqopo@gmail.com>
2025-05-28 15:11:36 +0900

    Fix Error

--- src/pages/BalanceGame/BalanceGame.tsx
+++ src/pages/BalanceGame/BalanceGame.tsx
@@ -58,7 +58,7 @@ export default function BalanceGame() {
         )
       ) : (
         /* 모든 질문에 아직 답변을 못 했다면 다음 질문 */
-        <div className='flex flex-col items-center bg-white p-8 rounded-lg w-5/6 min-h-[50vh]'>
+        <div className='flex flex-col items-center bg-white p-8 rounded-lg w-5/6 '>
           <div className='flex flex-col items-center'>
             <h2 className='text-md'>
               {results.length + 1}/{gameQuestions.length}
@@ -66,7 +66,7 @@ export default function BalanceGame() {
             <h2 className='text-xl sm:text-xl md:text-2xl my-4'>
               {gameQuestions[results.length].topic}
             </h2>
-            <div className='w-full flex flex-col space-y-8'>
+            <div className='w-full h-full flex flex-1 flex-col items-center justify-center space-y-8'>
               <BalanceChoiceButton
                 color='blue'
                 label={gameQuestions[results.length].selects.top.select}
